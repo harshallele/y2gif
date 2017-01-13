102596d1e0a461e5cb7ea59a7df74984c9c9bca0
Harshal Lele <frost17121997@gmail.com>
2017-01-13 22:33:13 +0530

    added section 2

--- public/js/main.js
+++ public/js/main.js
@@ -5,11 +5,29 @@ jQuery(document).ready(function(){
   //next button for first section
   var nextBtnSec1 = jQuery('#sec1-next');
 
+  //Section 1
+  var sec1 = jQuery('#sec1');
+  var sec2 = jQuery('#sec2');
+
+
+
+
+
+
+  var selectedVidUrl = '';
+
+
   //click listener for next button of first section
   nextBtnSec1.click(function(){
     if(ytUrlText.value) {
       //destroy the popover
       $('#yturl').popover('destroy');
+
+      selectedVidUrl = ytUrlText.value;
+
+      sec1.slideUp('fast');
+      sec2.slideDown('fast');
+
 
     }
     else {
