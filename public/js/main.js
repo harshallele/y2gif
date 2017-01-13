a68c6b598f8022e464b3e3047ce7973e0004e76f
Harshal Lele <frost17121997@gmail.com>
2017-01-13 17:10:06 +0530

    added comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,19 @@
 jQuery(document).ready(function(){
+
+  //youtube url text field
   var ytUrlText = jQuery('#yturl')[0];
+  //next button for first section
   var nextBtnSec1 = jQuery('#sec1-next');
 
+  //click listener for next button of first section
   nextBtnSec1.click(function(){
     if(ytUrlText.value) {
-
+      //destroy the popover
       $('#yturl').popover('destroy');
 
     }
     else {
+      //Show a popover asking the user to enter url
       $('#yturl').popover({
         title:'Empty URL',
         content:'Please paste Youtube URL',
