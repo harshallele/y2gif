27eba18ae70b832f254be959d3c4ffa1974c997f
Harshal Lele <frost17121997@gmail.com>
2017-01-15 00:37:31 +0530

    Added video player and video title in second section

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,75 @@
-jQuery(document).ready(function(){
+$(document).ready(function(){
 
   //youtube url text field
-  var ytUrlText = jQuery('#yturl')[0];
+  var ytUrlText = $('#yturl')[0];
   //next button for first section
-  var nextBtnSec1 = jQuery('#sec1-next');
+  var nextBtnSec1 = $('#sec1-next');
+
+  //Back button of second section
+  var prevBtnSec2 = $('#sec2-prev');
 
   //Section 1
-  var sec1 = jQuery('#sec1');
-  var sec2 = jQuery('#sec2');
+  var sec1 = $('#sec1');
+  var sec2 = $('#sec2');
 
   //The selected Youtube URL
   var selectedVidUrl = '';
+
+  //empty out the selected URL and display the first section
+  var loadFirstScreen = function(){
+
+    sec1.slideDown('fast');
+    sec2.slideUp('fast');
+
+    selectedVidUrl = '';
+  }
+
 
 
 
   //function to load the second screen with the video id, and
   var loadSecondScreen  = function() {
 
+    sec1.slideUp('fast');
+    sec2.slideDown('fast');
+
     var videoId = '';
     videoId = getYtId(selectedVidUrl);
-    console.log(videoId);
-    if(videoId === undefined | videoId === null |videoId == ''){
-      alert('URL is invalid');
+
+    $('#main-video').hide();
+    $('#video-title').show();
+
+    if(videoId === ''){
+      $('#video-title').text('Youtube URL is invalid');
+      $('#spinner').hide();
+    }
+    else{
+
+      //Use the Youtube Data Api to get the video title using the id
+      $.ajax({
+      url: "https://www.googleapis.com/youtube/v3/videos?id=" + videoId + "&key=AIzaSyCNEFLZTLyoR25zgHQxPMFCAvQtg3f8WF4&fields=items(snippet(title))&part=snippet",
+      dataType: "jsonp",
+      success: function(data){
+
+               //Set the video title and the video source
+               $('#video-title').text(data.items[0].snippet.title);
+               var mainPlayer = videojs('main-video');
+               mainPlayer.src({type: 'video/youtube', src: selectedVidUrl});
+               $('#main-video').show();
+               $('#main-video').width(sec2.width());
+               $('#main-video').height((sec2.width()*9)/16);
+               //Hide the loading spinner
+               $('#spinner').hide();
+
+             },
+      error: function(jqXHR, textStatus, errorThrown) {
+              $('#video-title').text(textStatus, + ' | ' + errorThrown);
+              //Hide the loading spinner
+              $('#spinner').hide();
+      }
+  });
+
+
     }
   }
 
@@ -35,10 +83,7 @@ jQuery(document).ready(function(){
       $('#yturl').popover('destroy');
 
       selectedVidUrl = ytUrlText.value;
-
-      sec1.slideUp('fast');
-      sec2.slideDown('fast');
-
+      //Load the second section
       loadSecondScreen();
 
     }
@@ -52,16 +97,24 @@ jQuery(document).ready(function(){
     }
   });
 
+  //Click listener for back button of second section
+  prevBtnSec2.click(function () {
+    loadFirstScreen();
+  });
 
+
+  //Get the youtube video id from the URL
   function getYtId(url){
     var ID = '';
     url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
     if(url[2] !== undefined) {
       ID = url[2].split(/[^0-9a-z_\-]/i);
       ID = ID[0];
+
     }
     else {
-      ID = url;
+      ID = '';
+
     }
       return ID;
 }
